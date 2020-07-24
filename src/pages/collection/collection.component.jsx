f7b2eacb952060324ef6d2a369612937d544252f
Khanhtran47 <tranduckhanh23@gmail.com>
2020-07-24 03:57:05 +0200

    converted collection-item, menu-item, directory and collection page over to styled-components

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,23 +1,29 @@
 import React from "react";
 import { connect } from "react-redux";
 
-import CollectionItem from "../../components/collection-item/collection-item.component";
-
 import { selectCollection } from "../../redux/shop/shop.selectors";
 
+import CollectionItem from "../../components/collection-item/collection-item.component";
+
 import "./collection.styles.scss";
+
+import {
+  CollectionPageContainer,
+  CollectionTitleContainer,
+  ItemsContainer,
+} from "./collection.styles";
 
 const CollectionPage = ({ collection }) => {
   const { title, items } = collection;
   return (
-    <div className="collection-page">
-      <h2 className="title">{title}</h2>
-      <div className="items">
+    <CollectionPageContainer>
+      <CollectionTitleContainer>{title}</CollectionTitleContainer>
+      <ItemsContainer>
         {items.map((item) => (
           <CollectionItem key={item.id} item={item} />
         ))}
-      </div>
-    </div>
+      </ItemsContainer>
+    </CollectionPageContainer>
   );
 };
 
